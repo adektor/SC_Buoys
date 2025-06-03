aa9c56c63f1289ec9b28bcd1a12b7917b33dc25d
Alec Dektor <adektor@lbl.gov>
2025-06-02 20:52:37 -0700

    add southern buoys

--- surf.js
+++ surf.js
@@ -37,8 +37,11 @@ const buoys = [
     { Name: 'Point Sur', lat: 36.34, lng: -122.1, id: '46239', uuid: '8f74b3c0-cecc-11eb-ad35-024238d3b313'},
     { Name: 'Monterey Canyon Outer', lat: 36.76, lng: -121.95, id: '46236', uuid: 'a7560458-df9d-11ef-ac15-029daffad6a3'},
     { Name: 'Cape San Martin', lat: 35.77, lng: -121.9, id: '46028', uuid: '48a1002e-cecd-11eb-ba0e-024238d3b313'},
+    { Name: 'Harvest', lat: 34.45, lng: -120.78, id: '46218', uuid: 'c795cab8-cecc-11eb-84e7-024238d3b313'},
+    { Name: 'Santa Lucia Escarpment', lat: 34.77, lng: -121.49, id: '46259', uuid: '599c6d1e-cecc-11eb-885c-024238d3b313'},
+    { Name: 'Diablo Canyon', lat: 35.2, lng: -120.86, id: '46215', uuid: 'a96cf346-cecb-11eb-8bb0-024238d3b313'},
     // { Name: 'Soquel Cove South', lat: 36.93, lng: -121.93, id: '46284', uuid: '3072ff0a-b656-11ef-94ee-066f3c48800f'},
-    { Name: 'Cabrillo Point', lat: 36.63, lng: -121.91, id: '46240', uuid: 'a3336676-cecd-11eb-9a27-024238d3b313'},
+    // { Name: 'Cabrillo Point', lat: 36.63, lng: -121.91, id: '46240', uuid: 'a3336676-cecd-11eb-9a27-024238d3b313'},
     // { Name: 'Monterey', lat: 36.79, lng: -122.4, id: '46042', uuid: 'dcfce512-cecb-11eb-b0c8-024238d3b313'},
     // { Name: 'Half Moon Bay', lat: 37.36, lng: -122.88, id: '46012', uuid: '780b2a02-cecb-11eb-9ccc-024238d3b313'},
     // { Name: 'Point Santa Cruz', lat: 36.93, lng: -122.03, id: '46269', uuid: 'a011a1e4-cecb-11eb-abf8-024238d3b313'},
@@ -47,7 +50,7 @@ const buoys = [
 let map; 
 
 const initializeLeafletMap = () => {
-  map = L.map('map', { center: [36.9, -122.5], zoom: 7.5 });
+  map = L.map('map', { center: [35.9, -122.5], zoom: 7.2 });
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
@@ -359,9 +362,9 @@ const periodToSwellType = (period) => {
     return { key: 'spws', Name: 'Short Period Windswell', color: '#0000ff' };
   } else if (period <= 9) {
     return { key: 'ws', Name: 'Windswell', color: '#00b7ff' };
-  } else if (period <= 12) {
+  } else if (period <= 13) {
     return { key: 'mps', Name: 'Mid Period Swell', color: '#ffca00' };
-  } else if (period <= 17) {
+  } else if (period <= 18) {
     return { key: 'gs', Name: 'Groundswell', color: '#ff8223' };
   } else {
     return { key: 'lpgs', Name: 'Long Period Groundswell', color: '#e60000' };
